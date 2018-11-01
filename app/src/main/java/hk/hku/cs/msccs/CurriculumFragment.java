d213ae1a72583a8f5573a83d45de56f5c8a0d1d0
XSYPaul <36841130+XSYPaul@users.noreply.github.com>
2018-11-01 15:13:29 +0800

    curriculum
    1101 afternoon

--- app/src/main/java/hk/hku/cs/msccs/CurriculumFragment.java
+++ app/src/main/java/hk/hku/cs/msccs/CurriculumFragment.java
@@ -3,11 +3,13 @@ package hk.hku.cs.msccs;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class CurriculumFragment extends Fragment {
@@ -17,8 +19,18 @@ public class CurriculumFragment extends Fragment {
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: ");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_curriculum, container, false);
+        View view= inflater.inflate(R.layout.fragment_curriculum, container, false);
+        return view;
     }
 
-
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+       /* ImageView titleImage = (ImageView) getActivity().findViewById(R.id.banner);//add getActivity(). before findViewById
+        ImageView cardimg1 = (ImageView) getActivity().findViewById(R.id.home_card_view1_image);
+        ImageView cardimg2 = (ImageView) getActivity().findViewById(R.id.home_card_view2_image);
+        ImageView cardimg3 = (ImageView) getActivity().findViewById(R.id.home_card_view3_image);
+        ImageView cardimg4 = (ImageView) getActivity().findViewById(R.id.home_card_view4_image);
+        */
+    }
 }
