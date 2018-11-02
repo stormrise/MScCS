578410dda7622d2950211be411ea69e4f29c7d0a
stormrise <lilingxiao0414@gmail.com>
2018-11-02 13:50:56 +0800

    redo & fix

--- app/src/main/java/hk/hku/cs/msccs/HomeFragment.java
+++ app/src/main/java/hk/hku/cs/msccs/HomeFragment.java
@@ -1,7 +1,10 @@
 package hk.hku.cs.msccs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.card.MaterialCardView;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,49 +25,60 @@ public class HomeFragment extends Fragment {
     private final static String TAG = "HomeActivity";
     private List<Guide_Content> card = new ArrayList<>();
     ImageView cardimg1, cardimg2, cardimg3, cardimg4;
-
-    private BGABanner bannerImage;
+    ImageView bannerImage;
+    //BGABanner bannerImage;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.d(TAG, "HomeActivity Start");
         //getFlow_text();
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {//equals mainActivity onCreated method
         super.onActivityCreated(savedInstanceState);
         //UI init must below this class
-        bannerImage = getActivity().findViewById(R.id.banner);//add getActivity(). before findViewById
+        //add getActivity(). before findViewById
+        bannerImage = getActivity().findViewById(R.id.banner);
         cardimg1 = (ImageView) getActivity().findViewById(R.id.home_card_view1_image);
         cardimg2 = (ImageView) getActivity().findViewById(R.id.home_card_view2_image);
         cardimg3 = (ImageView) getActivity().findViewById(R.id.home_card_view3_image);
         cardimg4 = (ImageView) getActivity().findViewById(R.id.home_card_view4_image);
-        //Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg").into(bannerImage);
-        //Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg").into(bannerImage);
-        Glide.with(cardimg1.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/brochureLatestFull.jpg").into(cardimg1);
-        Glide.with(cardimg2.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ProgrammeSchedule.jpg").into(cardimg2);
-        Glide.with(cardimg3.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ProgrammeFees.jpg").into(cardimg3);
-        Glide.with(cardimg4.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ApplicationDeadlines.jpg").into(cardimg4);
+//        Glide.with(bannerImage.getContext()).load(R.drawable.home_slider1).into(bannerImage);
+//        Glide.with(bannerImage.getContext()).load(R.drawable.home_slider_admission).into(bannerImage);
+//        Glide.with(cardimg1.getContext()).load(R.drawable.home_brochurelatestfull).into(cardimg1);
+//        Glide.with(cardimg2.getContext()).load(R.drawable.home_programme_schedule).into(cardimg2);
+//        Glide.with(cardimg3.getContext()).load(R.drawable.home_programme_fees).into(cardimg3);
+//        Glide.with(cardimg4.getContext()).load(R.drawable.home_application_deadlines).into(cardimg4);
+        //initBanner();
 
-        bannerImage.setAdapter(new BGABanner.Adapter<ImageView, String>() {
+        cardimg1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
-                Glide.with(bannerImage.getContext())
-                        .load(model)
-                        .into(itemView);
+            public void onClick(View v) {
+
             }
         });
 
-        bannerImage.setData(Arrays.asList("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg", "https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg"), Arrays.asList("Department of Computer Science", "Master of Science in Computer Science"));
-
-
     }
+
+//    void initBanner(){
+//        bannerImage.setAdapter(new BGABanner.Adapter<ImageView, String>() {
+//            @Override
+//            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
+//                Glide.with(bannerImage.getContext())
+//                        .load(model)
+//                        .into(itemView);
+//            }
+//        });
+//
+//        bannerImage.setData(
+//                Arrays.asList("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg", "https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg"),
+//                Arrays.asList("Department of Computer Science", "Master of Science in Computer Science"));
+//
+//    }
 
 }
