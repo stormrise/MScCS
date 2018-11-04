2fc41b9842184b5abd193c49bd975b9d46ae0863
XSYPaul <36841130+XSYPaul@users.noreply.github.com>
2018-11-04 23:57:58 +0800

    environment
    11.04.evening

---
+++ app/src/main/java/hk/hku/cs/msccs/Environment.java
@@ -0,0 +1,13 @@
+package hk.hku.cs.msccs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Environment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.res_environment);
+    }
+}
