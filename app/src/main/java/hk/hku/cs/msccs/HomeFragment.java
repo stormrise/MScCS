19eb2b3378abb4894fb8dc6b00e7f9ab53528fce
stormrise <lilingxiao0414@gmail.com>
2018-11-05 15:45:34 +0800

    seems fix banner

--- app/src/main/java/hk/hku/cs/msccs/HomeFragment.java
+++ app/src/main/java/hk/hku/cs/msccs/HomeFragment.java
@@ -29,8 +29,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private List<Guide_Content> card = new ArrayList<>();
     TextView bannerText;
     ImageView cardimg1, cardimg2, cardimg3, cardimg4;
-    ImageView bannerImage;
-    //BGABanner bannerImage;
+    //ImageView bannerImage;
+    BGABanner bannerImage;
     int count =0;
 
 
@@ -56,11 +56,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         cardimg4 = (ImageView) getActivity().findViewById(R.id.home_card_view4_image);
         //Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg").into(bannerImage);
         //Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg").into(bannerImage);
-        //Glide.with(cardimg1.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/brochureLatestFull.jpg").into(cardimg1);
-        //Glide.with(cardimg2.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ProgrammeSchedule.jpg").into(cardimg2);
-        //Glide.with(cardimg3.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ProgrammeFees.jpg").into(cardimg3);
-        //Glide.with(cardimg4.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ApplicationDeadlines.jpg").into(cardimg4);
-        //initBanner();
+        Glide.with(cardimg1.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/brochureLatestFull.jpg").into(cardimg1);
+        Glide.with(cardimg2.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ProgrammeSchedule.jpg").into(cardimg2);
+        Glide.with(cardimg3.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ProgrammeFees.jpg").into(cardimg3);
+        Glide.with(cardimg4.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/ContentImages/ApplicationDeadlines.jpg").into(cardimg4);
+        initBanner();
 
         bannerImage.setOnClickListener(this);
         cardimg1.setOnClickListener(this);
@@ -71,18 +71,18 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View v) {   //点击事件响应
         switch (v.getId()){
-            case R.id.banner:
-                if(count%2==1){
-                    bannerText.setText("Department of Computer Science 香港大學計算機系");
-                    Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg").into(bannerImage);
-                    count++;
-                }else {
-                    bannerText.setText("Master of Science in Computer Science 計算機科學理科碩士課程");
-                    Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg").into(bannerImage);
-                    count++;
-                }
-
-                break;
+//            case R.id.banner:
+//                if(count%2==1){
+//                    bannerText.setText("Department of Computer Science 香港大學計算機系");
+//                    Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg").into(bannerImage);
+//                    count++;
+//                }else {
+//                    bannerText.setText("Master of Science in Computer Science 計算機科學理科碩士課程");
+//                    Glide.with(bannerImage.getContext()).load("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg").into(bannerImage);
+//                    count++;
+//                }
+//
+//                break;
             case R.id.home_card_view1_image:
 
                 break;
@@ -98,20 +98,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         }
     }
 
-//    void initBanner(){
-//        bannerImage.setAdapter(new BGABanner.Adapter<ImageView, String>() {
-//            @Override
-//            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
-//                Glide.with(bannerImage.getContext())
-//                        .load(model)
-//                        .into(itemView);
-//            }
-//        });
-//
-//        bannerImage.setData(
-//                Arrays.asList("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg", "https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg"),
-//                Arrays.asList("Department of Computer Science", "Master of Science in Computer Science"));
-//
-//    }
+    void initBanner(){
+        bannerImage.setAdapter(new BGABanner.Adapter<ImageView, String>() {
+            @Override
+            public void fillBannerItem(BGABanner banner, ImageView itemView, String model, int position) {
+                Glide.with(bannerImage.getContext())
+                        .load(model)
+                        .into(itemView);
+            }
+        });
+
+        bannerImage.setData(
+                Arrays.asList("https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider1.jpg", "https://www.msc-cs.hku.hk/Media/Default/Slider%20Images/Slider_Admission.jpg"),
+                Arrays.asList("Department of Computer Science", "Master of Science in Computer Science"));
+
+    }
 
 }
