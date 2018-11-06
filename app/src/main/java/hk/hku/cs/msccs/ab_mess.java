cb0f2bbb6a213cb66b9715b49b71f4fc85484116
Dan-lord <729266076@qq.com>
2018-11-06 13:33:03 +0800

    about_mess
    about_mess

---
+++ app/src/main/java/hk/hku/cs/msccs/ab_mess.java
@@ -0,0 +1,13 @@
+package hk.hku.cs.msccs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ab_mess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ab_mess);
+    }
+}
