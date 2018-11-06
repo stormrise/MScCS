e7c6337d8d1b97428931491d5f616f418c4d75cd
Dan-lord <729266076@qq.com>
2018-11-06 22:49:50 +0800

    final admission+about
    more content

---
+++ app/src/main/java/hk/hku/cs/msccs/ad_6.java
@@ -0,0 +1,13 @@
+package hk.hku.cs.msccs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ad_6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_6);
+    }
+}
